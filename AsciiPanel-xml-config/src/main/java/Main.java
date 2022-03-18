1928d59c0fbd6b8d426be997ca47990d9d9b0dfe
yanghaku <1961882079@qq.com>
2022-03-18 14:54:10 +0800

    finish asciiPanel xml

---
+++ AsciiPanel-xml-config/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import asciiPanel.AsciiPanel;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        // 读取xml文件进行装载注入
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        // 获取装载的asciiPanel实例
+        AsciiPanel asciiPanel = context.getBean("asciiPanelTest", AsciiPanel.class);
+
+        System.out.println("now the font name = " + asciiPanel.getAsciiFont().getFontFilename());
+    }
+}
